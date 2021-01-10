420281f17e1202742298478a8ab024362ada3e26
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-01-10 09:56:38 +0100

    cd..

--- app/src/main/java/com/com/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/com/constraintlayout/MainActivity.kt
@@ -7,19 +7,24 @@ import androidx.transition.TransitionManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    val constrainsSet1 = ConstraintSet()
-    val constrainsSet2 = ConstraintSet()
+    val constraintSet1 = ConstraintSet()
+    val constraintSet2 = ConstraintSet()
+
+    var setBool = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        constrainsSet1.clone(this,R.layout.activity_main)
-        constrainsSet2.clone(this,R.layout.activity_main_1)
+        constraintSet1.clone(this,R.layout.activity_main)
+        constraintSet2.clone(this,R.layout.activity_main_1)
 
         button0.setOnClickListener{
             TransitionManager.beginDelayedTransition(constraintLayout)
-            constrainsSet2.apply { constraintLayout }
+            val currentSet = if(setBool)constraintSet1 else constraintSet2
+
+            setBool = !setBool
+            currentSet.applyTo(constraintLayout)
         }
 
     }
