ec18a196683093bae6f7c4d16a988dd0f79571cf
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-01-10 09:38:30 +0100

    cd..

--- app/src/main/java/com/com/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/com/constraintlayout/MainActivity.kt
@@ -2,10 +2,25 @@ package com.com.constraintlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.transition.TransitionManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val constrainsSet1 = ConstraintSet()
+    val constrainsSet2 = ConstraintSet()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        constrainsSet1.clone(this,R.layout.activity_main)
+        constrainsSet2.clone(this,R.layout.activity_main_1)
+
+        button0.setOnClickListener{
+            TransitionManager.beginDelayedTransition(constraintLayout)
+            constrainsSet2.apply { constraintLayout }
+        }
+
     }
 }
